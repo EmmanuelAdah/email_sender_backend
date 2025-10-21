156c5ff8bb397a2a7d370a5256f06dc8fb17993b
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-21 18:53:04 +0100

    recipient entity for its attributes

--- src/main/java/com/emailsender/data/models/Recipient.java
+++ src/main/java/com/emailsender/data/models/Recipient.java
@@ -3,14 +3,17 @@ package com.emailsender.data.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Generated;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Recipient {
+    @Id
+    @Generated
+    private long id;
     private String email;
     private String name;
-    private String subject;
-    private String message;
 }
