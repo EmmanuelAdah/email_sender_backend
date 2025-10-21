05c6939cf53ef59995422ab998e5084a286fe0e8
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-21 17:07:04 +0100

    users repository

---
+++ src/main/java/com/emailsender/data/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.emailsender.data.repositories;
+
+import com.emailsender.data.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
