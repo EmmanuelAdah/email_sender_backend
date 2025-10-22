3b7f4e7120156cb32e6570f667ce70741e754193
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-22 17:18:28 +0100

    user entity for its fields

--- src/main/java/com/emailsender/data/models/User.java
+++ src/main/java/com/emailsender/data/models/User.java
@@ -1,25 +1,69 @@
 package com.emailsender.data.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Generated;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Data
 @Component
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+    @jakarta.persistence.Id
     @Id
     @Generated
-    private String id;
+    private long id;
     private String email;
     private String username;
     private String password;
 
+    @OneToMany(mappedBy = "userId", fetch = FetchType.LAZY )
     List<EmailHistory> emailHistory = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
